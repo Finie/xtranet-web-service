8a08b338acd981e28252fa1a4d5d86c0d4428770
unknown <finiephenius@gmail.com>
2021-01-19 15:22:35 +0300

    auth update

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ module.exports = function (req, res, next) {
   if (!token)
     return res.status(401).send({
       status: "request failed",
-      description: "Access Denied No Auth Token so you are not authorised to perorm that operation",
+      description: "Access Denied No Auth Token ",
     });
 
   try {
